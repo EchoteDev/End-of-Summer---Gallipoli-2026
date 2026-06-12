5dc6092569fc75e31bfaa95c1f903db04d1cf755
Jamal Echote <jam@Jamals-MacBook-Pro-2.local>
2026-06-12 14:41:44 +0200

    Switch to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Landing from './pages/Landing';
 import BookingWizard from './pages/BookingWizard';
@@ -12,15 +12,15 @@ import AdminDashboard from './pages/AdminDashboard';
 
 export default function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Landing />} />
-          <Route path="booking" element={<BookingWizard />} />
-          <Route path="customer" element={<CustomerArea />} />
-          <Route path="admin" element={<AdminDashboard />} />
+           <Route index element={<Landing />} />
+           <Route path="booking" element={<BookingWizard />} />
+           <Route path="customer" element={<CustomerArea />} />
+           <Route path="admin" element={<AdminDashboard />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
